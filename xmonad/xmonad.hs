f7ac74af625f5019bd51db6f5b3e0a2e8d181c1f
Natxo Cabré <natxo.cabre@gmail.com>
2011-05-03 23:32:26 +0200

    changed xmonad and xmobarrc. At this moment, xmonad is set up with xmobar, trayer under ubuntu.

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -3,47 +3,77 @@ import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.UrgencyHook
+import XMonad.Layout.Named
 import XMonad.Layout.LayoutHints
 import XMonad.Layout.NoBorders
 import XMonad.Layout.PerWorkspace
+import XMonad.Layout.IM
+import XMonad.Layout.ToggleLayouts
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
+import qualified XMonad.StackSet as W
+import Data.Ratio ((%))
 
 main = do
-  xmproc <- spawnPipe "xmobar /home/mitchellh/.xmobarrc"
+  -- launch xmobar as status bar
+  xmproc <- spawnPipe "xmobar ~/.xmobarrc"
   xmonad $ withUrgencyHook NoUrgencyHook $ defaultConfig
-    { terminal = "urxvt"
+    { -- use urxvt client as terminal
+      terminal = "urxvtc"
+      -- use Win key
     , modMask = mod4Mask
     , normalBorderColor = "#1F1F1F"
     , focusedBorderColor = "#2688DE"
     , workspaces = myWorkspaces
     , manageHook = manageHook defaultConfig <+> myManageHook
-    , layoutHook = myLayout
+    , layoutHook = myLayoutHook
     , logHook = dynamicLogWithPP $ xmobarPP
                 { ppOutput = hPutStrLn xmproc
                 , ppTitle = xmobarColor "green" "" . shorten 50
+ 		-- hide layout name
+		, ppLayout = const ""
                 }
     } `additionalKeys`
-    [ ((mod4Mask .|. shiftMask, xK_z), spawn "gnome-screensaver-command --lock")
+    [
+	  -- suspend key: win + F1
+	  -- add USER ALL=NOPASSWD:ALL in /etc/sudoers
+	  ((mod4Mask, xK_F1), spawn "sudo pm-suspend"),
+	  -- lock screen key: win + shift + L
+	  ((mod4Mask .|. shiftMask, xK_l), spawn "gnome-screensaver-command --lock")
     ]
 
-myWorkspaces = ["1:web", "2:chat", "3:edit", "4", "5", "6"]
+myWorkspaces = ["1:dev", "2:web", "3:im", "4:vm", "5:media", "6:others"]
 
-myLayout = avoidStruts $ layoutHints $
-           onWorkspaces ["1:web", "2:chat", "3:edit"] Full $
-           (Full ||| tiled (1/2))
-  where
-    tiled = Tall 1 (3/100)
+myManageHook = composeAll
+    [ className =? "Gimp" --> doFloat 
+     -- move piding to IM workspace 
+     , className =? "Pidgin"         --> moveTo "3:im"     
+     -- move virtualbox to 4:vm
+     , className =? "VirtualBox"         --> moveTo "4:vm"
+     -- vboxmanage is used to launch directly a VM
+     , className =? "VBoxManage"         --> moveTo "4:vm"
+     -- xmonad + trayer on all workspaces 
+     , manageDocks
+    ]
+    where moveTo = doF . W.shift
 
-myManageHook = composeAll . concat $
-               [ [isDialog --> doFloat]
-               , [isA x --> doShift "1:web" | x <- my1Shifts]
-               , [isA x --> doShift "2:chat" | x <- my2Shifts]
-               , [isA x --> doShift "3:edit" | x <- my3Shifts]
-               ]
-  where
-    isA x = className =? x <||> title =? x <||> resource =? x <||> appName =? x
-    my1Shifts = ["chromium-browser"]
-    my2Shifts = ["irssi"]
-    my3Shifts = ["emacs"]
+
+-- layouts
+-- imLayout used for pidgin
+imLayout = avoidStruts $ IM (1%7) (Or (And (ClassName "Pidgin") (Role "buddy_list"))
+                         (And (ClassName "Skype")  (And (Role "") (Not (Title "Options")))))
+
+-- layouts
+-- extrated from http://www.haskell.org/haskellwiki/Xmonad/Config_archive/Thomas_ten_Cate's_xmonad.hs
+basicLayout = Tall nmaster delta ratio where
+    nmaster = 1
+    delta   = 3/100
+    ratio   = 1/2
+tallLayout = named "tall" $ avoidStruts $ basicLayout
+wideLayout = named "wide" $ avoidStruts $ Mirror basicLayout
+singleLayout = named "single" $ avoidStruts $ noBorders Full
+fullscreenLayout = named "fullscreen" $ noBorders Full
+myLayoutHook = im $ normal where
+    normal     = tallLayout ||| wideLayout ||| singleLayout ||| fullscreenLayout
+    im         = onWorkspace "3:im" imLayout
