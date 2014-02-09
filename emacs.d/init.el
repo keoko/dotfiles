21ee6a263ab57db101b1e070f3e8e7ef69937585
Ignacio Cabre Barrera <cabrei@gsicommece.com>
2014-02-09 12:40:01 +0100

    changed init.el to include smex package

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,60 +1,90 @@
-;;-----------------------------------------------------------------
-;; Default Variables (Global)
-;;-----------------------------------------------------------------
-(setq-default indent-tabs-mode nil)
-(setq-default indicate-empty-lines t)
-(setq-default tab-width 2)
-(setq-default espresso-indent-level 2)
-(setq-default vc-handled-backends nil)
-(defalias 'yes-or-no-p 'y-or-n-p)
-(setq-default column-number-mode t)
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 
-;;-----------------------------------------------------------------
-;; Load paths and folder variables
-;;-----------------------------------------------------------------
-;; Variables to help
-(setq dotfiles-dir (file-name-directory
-                    (or (buffer-file-name) load-file-name)))
-(setq packages-dir (concat dotfiles-dir "/packages"))
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
 
-;; Add this directory to load path initially
-(add-to-list 'load-path dotfiles-dir)
+(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+; (el-get 'sync)
 
-;; Don't clutter up directories with files~
-(setq backup-directory-alist `(("." . ,(expand-file-name
-                                        (concat dotfiles-dir "backups")))))
-(setq auto-save-default nil)
+(setq my-packages
+      (append
+       '(ace-jump-mode yaml-mode color-theme color-theme-solarized
+		       markdown-mode htmlize
+		       clojure-mode cider paredit
+		       deft
+		       multiple-cursors highline smex
+		       zenburn-theme)
 
-;;-----------------------------------------------------------------
-;; Display Options
-;;-----------------------------------------------------------------
-(load "display.el")
+       (mapcar 'el-get-as-symbol (mapcar 'el-get-source-name el-get-sources))))
 
+(el-get 'sync my-packages)
+
+
+;; decor
+(setq inhibit-startup-screen t)
+(menu-bar-mode -1)
 (if window-system
-    (load "window.el")
-    (load "terminal.el"))
+    (tool-bar-mode -1))
+(load-theme 'zenburn t)
 
-;;-----------------------------------------------------------------
-;; Other Packages and Files
-;;-----------------------------------------------------------------
-(load "modes.el")
-(load "keybindings.el")
-(load "hooks.el")
-(load "filetypes.el")
 
-;; ido
+;; deft
+(setq deft-directory "~/Dropbox/notes")
+(setq deft-extension "org")
+(setq deft-text-mode 'org-mode)
+
+(let ((destination (if (file-exists-p "~/Dropbox")
+                       "~/Dropbox/notes.org"
+                     "~/notes.org")))
+  (setq org-default-notes-file destination))
+(global-set-key (kbd "C-c d") 'deft)
+
+
+;; backups
+(setq backup-by-copying t
+      backup-directory-alist '(("." . "~/.emacs.d/backup"))
+      delete-old-versions t
+      kept-new-versions 128
+      kept-old-versions 128
+      version-control t)
+
+;; ace-jump-mode
+(require 'ace-jump-mode)
+(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
+
+
+;; ido-mode
+(require 'ido)
 (ido-mode t)
-(setq ido-enable-flex-matching t)
+(setq ido-auto-merge-work-directories-length nil
+      ido-create-new-buffer 'always
+      ido-enable-flex-matching t
+      ido-enable-prefix nil
+      ido-handle-duplicate-virtual-buffers 2
+      ido-max-prospects 10
+      ido-use-filename-at-point 'guess
+      ido-use-virtual-buffers t)
+(setq ido-everywhere t)
+(defalias 'yes-or-no-p 'y-or-n-p)
 
-;;-----------------------------------------------------------------
-;; System/User-specific configuration
-;;-----------------------------------------------------------------
-;; Loads system-type config first. e.g. "darwin.el" on mac
-(let ((system-type-config (concat dotfiles-dir (symbol-name system-type) ".el")))
-  (if (file-exists-p system-type-config)
-    (load system-type-config)))
 
-;; Load user-specific config next
-(let ((user-login-config (concat dotfiles-dir (downcase user-login-name) ".el")))
-  (if (file-exists-p user-login-config)
-    (load user-login-config)))
+;; linum
+;;(require 'linum)
+;;(global-linum-mode t)
+;;(setq linum-format "%2d ")
+
+
+;; multiple cursors
+(require 'multiple-cursors)
+(global-set-key (kbd "C-c C-s") 'mc/edit-lines)
+
+
+; smex Alt-x improved
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+;; This is your old M-x.
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
