0e6acf24ca05b760d1d067ee1ec81baa5795c11b
Natxo Cabré <natxo.cabre@magento.com>
2017-10-27 20:58:54 +0200

    paredit

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,9 +1,14 @@
+;; TODO - check https://github.com/manuel-uberti/.emacs.d
+;; TODO - check emacs-live
+;; TODO - autocomplete
 ;; TODO - helm notes
 ;; TODO - theme
 ;; TODO - multicursor
 ;; TODO - install cider
 ;; TODO - install clojurescript
 ;; TODO - install clojure
+;; TODO - pretify XML, json files
+;; TODO - rebin top line, bottom line
 
 ;; disable menu bar, scroll bar and tool bar
 (menu-bar-mode -1)
@@ -28,7 +33,7 @@
 ;;;;
 ;; Packages
 ;;;;
-;; Define package repositories
+;; define package repositories
 (require 'package)
 (add-to-list 'package-archives
              '("gnu" . "http://elpa.gnu.org/packages/") t)
@@ -37,31 +42,15 @@
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/") t)
 
-;; (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-;;                          ("marmalade" . "http://marmalade-repo.org/packages/")
-;;                          ("melpa" . "http://melpa-stable.milkbox.net/packages/")))
-
-
-;; Load and activate emacs packages. Do this first so that the
-;; packages are loaded before you start trying to modify them.
-;; This also sets the load path.
+;; sets the load path
 (package-initialize)
 
-;; Download the ELPA archive description if needed.
-;; This informs Emacs about the latest versions of all packages, and
-;; makes them available for download.
 (when (not package-archive-contents)
   (package-refresh-contents))
 
-;; The packages you want installed. You can also install these
-;; manually with M-x package-install
-;; Add in your own as you wish:
 (defvar my-packages
-  '(;; makes handling lisp expressions much, much easier
-    ;; Cheatsheet: http://www.emacswiki.org/emacs/PareditCheatsheet
-    ;; paredit
-
-    cider
+  '(;; cheatsheet: http://www.emacswiki.org/emacs/PareditCheatsheet
+    paredit
 
     ;; key bindings and code colorization for Clojure
     ;; https://github.com/clojure-emacs/clojure-mode
@@ -72,7 +61,7 @@
 
     ;; integration with a Clojure REPL
     ;; https://github.com/clojure-emacs/cider
-    ;; cider
+    cider
 
     ;; allow ido usage in as many contexts as possible. see
     ;; customizations/navigation.el line 23 for a description
@@ -112,6 +101,8 @@
     ;; helm
     ;;helm
     ))
+
+my-packages
 
 ;; On OS X, an Emacs instance started from the graphical user
 ;; interface will have a different environment than a shell in a
@@ -209,3 +200,6 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+
+(setq cider-lein-command "/Users/icabrebarrera/bin/lein")
